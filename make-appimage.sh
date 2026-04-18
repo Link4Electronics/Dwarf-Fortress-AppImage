6cfc2a1847d5864d456ea07801fb38bc12ea44bb
Link4Electronics <link4electronics@gmail.com>
2026-04-18 20:56:35 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,11 +9,10 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/dwarffortress.png
-export DESKTOP=/usr/share/applications/dwarffortress.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/dwarffortress /opt/dwarffortress
+quick-sharun /opt/dwarffortress/dwarfort /opt/dwarffortress
 
 # Additional changes can be done in between here
 
